d1d1700a69eb40d786dfdb7c8d2b5e1c156fb812
Sudarshan Rajagopalan <67941681+suraj7026@users.noreply.github.com>
2025-01-08 15:33:51 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get update
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
